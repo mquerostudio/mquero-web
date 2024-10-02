a7a18f25d9bdb3d38d413f199aadd14a400b579d
mquerostudio <62671749+mquerostudio@users.noreply.github.com>
2024-10-02 17:22:40 +0000

    change

--- frontend/src/app/(auth)/signin/page.tsx
+++
@@ -1,5 +0,0 @@
-import { SigninForm } from "@/components/forms/SigninForms";
-
-export default function SignInRoute() {
-    return <SigninForm />;
-}
--- frontend/src/app/(auth)/signup/page.tsx
+++
@@ -1,5 +0,0 @@
-import { SignupForm } from "@/components/forms/SignupForms";
-
-export default function SignUpRoute() {
-    return <SignupForm />;
-}
