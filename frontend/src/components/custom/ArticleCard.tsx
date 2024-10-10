bbdd87fcf2af338867ff85af2ddac7ef792ba16e
mquerostudio <62671749+mquerostudio@users.noreply.github.com>
2024-10-10 08:48:36 +0000

    fix img placeholders

--- frontend/src/components/custom/ArticleCard.tsx
+++ frontend/src/components/custom/ArticleCard.tsx
@@ -53,7 +53,7 @@ export function ArticleCard({
                     <div className="animate-pulse">
                         <div className="relative w-full h-52 overflow-hidden">
                             <StrapiImage
-                                src={"uploads/img_placeholder_9590864e93.png"}
+                                src={"https://optimistic-kindness-c344eba2ae.media.strapiapp.com/img_placeholder_9590864e93.png"}
                                 alt={"Placeholder image"}
                                 width={1200}
                                 height={800}
--- frontend/src/components/custom/ProjectCard.tsx
+++ frontend/src/components/custom/ProjectCard.tsx
@@ -44,7 +44,7 @@ export function ProjectCard({ project }: { project?: ProjectProps }) {
             <div className="animate-pulse bg-white">
                 <div className="relative w-full h-52 overflow-hidden">
                     <StrapiImage
-                        src={"/img_placeholder_9590864e93.png"}
+                        src={"https://optimistic-kindness-c344eba2ae.media.strapiapp.com/img_placeholder_9590864e93.png"}
                         alt={"Placeholder image"}
                         width={1200}
                         height={800}
