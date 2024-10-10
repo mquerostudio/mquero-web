e8a64838180297ee9b30cccc64715fa1f8415fa4
mquerostudio <62671749+mquerostudio@users.noreply.github.com>
2024-10-10 16:45:06 +0000

    new

--- frontend/src/app/about-me/page.tsx
+++ frontend/src/app/about-me/page.tsx
@@ -1,7 +1,16 @@
 export default function AboutMe() {
     return (
-        <div>
-            <h1>About Me</h1>
-        </div>
+        <main>
+            <header className="py-24">
+                <div className="max-w-[1348px] w-full flex flex-col justify-between items-start mx-auto space-y-4 lg:space-y-0 h-auto">
+                    <h1 className="text-2xl sm:text-4xl font-bold w-full">
+                        Próximamente...
+                    </h1>
+                    {/* <p className="text-2xl">
+                    {description}
+                </p> */}
+                </div>
+            </header>
+        </main>
     );
 }
