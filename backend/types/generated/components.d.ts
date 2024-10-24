2f9db7b51644df3c15a016bf4bfb175c7567ddf4
mquerostudio <62671749+mquerostudio@users.noreply.github.com>
2024-10-24 11:32:40 +0000

    update version

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,147 +1,26 @@
-import type { Struct, Schema } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface LayoutSkillSection extends Struct.ComponentSchema {
-  collectionName: 'components_layout_skill_sections';
+export interface ComponentsFilter extends Struct.ComponentSchema {
+  collectionName: 'components_components_filters';
   info: {
-    displayName: 'Skills Section';
-    description: '';
+    displayName: 'filter';
   };
   attributes: {
-    skill: Schema.Attribute.Component<'components.skill', true>;
-    heading: Schema.Attribute.String;
+    heading: Schema.Attribute.String & Schema.Attribute.Required;
+    subheading: Schema.Attribute.String & Schema.Attribute.Required;
   };
 }
 
-export interface LayoutLatestProjects extends Struct.ComponentSchema {
-  collectionName: 'components_layout_latest_projects';
+export interface ComponentsFooterCol1 extends Struct.ComponentSchema {
+  collectionName: 'components_components_footer_col1s';
   info: {
-    displayName: 'Latest Projects Section';
     description: '';
+    displayName: 'Footer Column One';
   };
   attributes: {
     heading: Schema.Attribute.String;
-    button: Schema.Attribute.Component<'components.link', false>;
-  };
-}
-
-export interface LayoutLatestArticlesSection extends Struct.ComponentSchema {
-  collectionName: 'components_layout_latest_articles_sections';
-  info: {
-    displayName: 'Latest Articles Section';
-  };
-  attributes: {
-    heading: Schema.Attribute.String;
-    button: Schema.Attribute.Component<'components.link', false>;
-  };
-}
-
-export interface LayoutHeroSection extends Struct.ComponentSchema {
-  collectionName: 'components_layout_hero_sections';
-  info: {
-    displayName: 'Hero Section';
-    description: '';
-  };
-  attributes: {
-    heading: Schema.Attribute.String;
-    subHeading: Schema.Attribute.Text;
-    image: Schema.Attribute.Media<'images'>;
-    button: Schema.Attribute.Component<'components.link', false>;
-  };
-}
-
-export interface LayoutHeader extends Struct.ComponentSchema {
-  collectionName: 'components_layout_headers';
-  info: {
-    displayName: 'Header';
-    description: '';
-  };
-  attributes: {
     logo: Schema.Attribute.Component<'components.logo', false>;
-    headerLink: Schema.Attribute.Component<'components.link', true>;
-  };
-}
-
-export interface LayoutFooter extends Struct.ComponentSchema {
-  collectionName: 'components_layout_footers';
-  info: {
-    displayName: 'Footer';
-    description: '';
-  };
-  attributes: {
-    footerColumnOne: Schema.Attribute.Component<
-      'components.footer-col1',
-      false
-    >;
-    footerColumnTwo: Schema.Attribute.Component<
-      'components.footer-column-two',
-      false
-    >;
-    footerColumnThree: Schema.Attribute.Component<
-      'components.footer-column-three',
-      false
-    >;
-  };
-}
-
-export interface ComponentsSkill extends Struct.ComponentSchema {
-  collectionName: 'components_components_skills';
-  info: {
-    displayName: 'Skill';
-    icon: '';
-    description: '';
-  };
-  attributes: {
-    heading: Schema.Attribute.String;
-    subHeading: Schema.Attribute.Text;
-    icon: Schema.Attribute.String;
-  };
-}
-
-export interface ComponentsSeo extends Struct.ComponentSchema {
-  collectionName: 'components_components_seos';
-  info: {
-    displayName: 'Seo';
-  };
-  attributes: {
-    MetaTitle: Schema.Attribute.String & Schema.Attribute.Required;
-    MetaDescription: Schema.Attribute.String & Schema.Attribute.Required;
-  };
-}
-
-export interface ComponentsLogo extends Struct.ComponentSchema {
-  collectionName: 'components_components_logos';
-  info: {
-    displayName: 'logo';
-    description: '';
-  };
-  attributes: {
-    url: Schema.Attribute.String;
-    image: Schema.Attribute.Media<'images'>;
-    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     text: Schema.Attribute.String;
-  };
-}
-
-export interface ComponentsLink extends Struct.ComponentSchema {
-  collectionName: 'components_components_links';
-  info: {
-    displayName: 'Link';
-  };
-  attributes: {
-    url: Schema.Attribute.String;
-    text: Schema.Attribute.String;
-    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
-  };
-}
-
-export interface ComponentsFooterColumnTwo extends Struct.ComponentSchema {
-  collectionName: 'components_components_footer_column_twos';
-  info: {
-    displayName: 'Footer Column Two';
-    description: '';
-  };
-  attributes: {
-    heading: Schema.Attribute.String;
   };
 }
 
@@ -156,47 +35,168 @@ export interface ComponentsFooterColumnThree extends Struct.ComponentSchema {
   };
 }
 
-export interface ComponentsFooterCol1 extends Struct.ComponentSchema {
-  collectionName: 'components_components_footer_col1s';
+export interface ComponentsFooterColumnTwo extends Struct.ComponentSchema {
+  collectionName: 'components_components_footer_column_twos';
   info: {
-    displayName: 'Footer Column One';
     description: '';
+    displayName: 'Footer Column Two';
   };
   attributes: {
     heading: Schema.Attribute.String;
-    logo: Schema.Attribute.Component<'components.logo', false>;
-    text: Schema.Attribute.String;
   };
 }
 
-export interface ComponentsFilter extends Struct.ComponentSchema {
-  collectionName: 'components_components_filters';
+export interface ComponentsLink extends Struct.ComponentSchema {
+  collectionName: 'components_components_links';
   info: {
-    displayName: 'filter';
+    displayName: 'Link';
   };
   attributes: {
-    heading: Schema.Attribute.String & Schema.Attribute.Required;
-    subheading: Schema.Attribute.String & Schema.Attribute.Required;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    text: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface ComponentsLogo extends Struct.ComponentSchema {
+  collectionName: 'components_components_logos';
+  info: {
+    description: '';
+    displayName: 'logo';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images'>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    text: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface ComponentsSeo extends Struct.ComponentSchema {
+  collectionName: 'components_components_seos';
+  info: {
+    displayName: 'Seo';
+  };
+  attributes: {
+    MetaDescription: Schema.Attribute.String & Schema.Attribute.Required;
+    MetaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface ComponentsSkill extends Struct.ComponentSchema {
+  collectionName: 'components_components_skills';
+  info: {
+    description: '';
+    displayName: 'Skill';
+    icon: '';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    icon: Schema.Attribute.String;
+    subHeading: Schema.Attribute.Text;
+  };
+}
+
+export interface LayoutFooter extends Struct.ComponentSchema {
+  collectionName: 'components_layout_footers';
+  info: {
+    description: '';
+    displayName: 'Footer';
+  };
+  attributes: {
+    footerColumnOne: Schema.Attribute.Component<
+      'components.footer-col1',
+      false
+    >;
+    footerColumnThree: Schema.Attribute.Component<
+      'components.footer-column-three',
+      false
+    >;
+    footerColumnTwo: Schema.Attribute.Component<
+      'components.footer-column-two',
+      false
+    >;
+  };
+}
+
+export interface LayoutHeader extends Struct.ComponentSchema {
+  collectionName: 'components_layout_headers';
+  info: {
+    description: '';
+    displayName: 'Header';
+  };
+  attributes: {
+    headerLink: Schema.Attribute.Component<'components.link', true>;
+    logo: Schema.Attribute.Component<'components.logo', false>;
+  };
+}
+
+export interface LayoutHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    description: '';
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    button: Schema.Attribute.Component<'components.link', false>;
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    subHeading: Schema.Attribute.Text;
+  };
+}
+
+export interface LayoutLatestArticlesSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_latest_articles_sections';
+  info: {
+    displayName: 'Latest Articles Section';
+  };
+  attributes: {
+    button: Schema.Attribute.Component<'components.link', false>;
+    heading: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutLatestProjects extends Struct.ComponentSchema {
+  collectionName: 'components_layout_latest_projects';
+  info: {
+    description: '';
+    displayName: 'Latest Projects Section';
+  };
+  attributes: {
+    button: Schema.Attribute.Component<'components.link', false>;
+    heading: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutSkillSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_skill_sections';
+  info: {
+    description: '';
+    displayName: 'Skills Section';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    skill: Schema.Attribute.Component<'components.skill', true>;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'layout.skill-section': LayoutSkillSection;
-      'layout.latest-projects': LayoutLatestProjects;
-      'layout.latest-articles-section': LayoutLatestArticlesSection;
-      'layout.hero-section': LayoutHeroSection;
-      'layout.header': LayoutHeader;
-      'layout.footer': LayoutFooter;
-      'components.skill': ComponentsSkill;
-      'components.seo': ComponentsSeo;
-      'components.logo': ComponentsLogo;
-      'components.link': ComponentsLink;
-      'components.footer-column-two': ComponentsFooterColumnTwo;
-      'components.footer-column-three': ComponentsFooterColumnThree;
-      'components.footer-col1': ComponentsFooterCol1;
       'components.filter': ComponentsFilter;
+      'components.footer-col1': ComponentsFooterCol1;
+      'components.footer-column-three': ComponentsFooterColumnThree;
+      'components.footer-column-two': ComponentsFooterColumnTwo;
+      'components.link': ComponentsLink;
+      'components.logo': ComponentsLogo;
+      'components.seo': ComponentsSeo;
+      'components.skill': ComponentsSkill;
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
+      'layout.hero-section': LayoutHeroSection;
+      'layout.latest-articles-section': LayoutLatestArticlesSection;
+      'layout.latest-projects': LayoutLatestProjects;
+      'layout.skill-section': LayoutSkillSection;
     }
   }
 }
