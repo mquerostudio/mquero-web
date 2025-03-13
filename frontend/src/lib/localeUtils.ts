4adff971c5c9d65459d4359711b9e6e1df86251d
MQuero <mquerostudio@gmail.com>
2025-03-13 09:44:04 +0100

    add new branch for web 2.0

--- frontend/src/lib/localeUtils.ts
+++
@@ -1,7 +0,0 @@
-import { headers } from "next/headers";
-
-export function getLocaleFromHost(): "en" | "es" {
-    const host = headers().get("host") || "";
-    return host.endsWith(".es") ? "es" : "en";
-}
-
